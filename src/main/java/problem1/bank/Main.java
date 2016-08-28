58a3c063f5e8a590a8f17a54987f36d810631fe3
kennylbj <632786081@qq.com>
2016-08-28 18:14:29 +0800

    add EffectiveCustomer version for problem 1

--- src/main/java/problem1/bank/Main.java
+++ src/main/java/problem1/bank/Main.java
@@ -15,7 +15,8 @@ public class Main {
     private static final int CUSTOMER_NUM = 10;
     private static final int THRESHOLD = 10;
     private static final int MONEY = 200000;
-    private static final int WAIT = 2 * 1000;
+    private static final int SLEEP_TIME = 10 * 1000;
+
     public static void main(String[] args) throws InterruptedException {
         //init accounts
         Map<Integer, Integer> accounts = new HashMap<>(CUSTOMER_NUM);
@@ -30,32 +31,48 @@ public class Main {
         }
 
         //start thread
-        for (int i = 0; i < CUSTOMER_NUM; i++) {
-            new Thread(customers[i]).start();
-        }
+        Arrays.stream(customers).forEach(customer -> new Thread(customer).start());
 
         //wait a bit of time
-        Thread.sleep(WAIT);
+        Thread.sleep(SLEEP_TIME);
 
         //stop all threads
-        for (int i = 0; i < CUSTOMER_NUM; i++) {
-            customers[i].stop();
-        }
+        Arrays.stream(customers).forEach(Customer::stop);
 
-        accounts.entrySet().forEach(System.out::println);
 
         int sum = accounts.values()
                 .stream()
                 .mapToInt(Integer::intValue)
                 .sum();
-        System.out.println("now the sum is " + sum);
         System.out.println("we expected to be " + (MONEY * CUSTOMER_NUM));
+        System.out.println("normal customer sum is " + sum);
+        assert sum == MONEY*CUSTOMER_NUM;
 
         int totalCount = Arrays.stream(customers).mapToInt(Customer::getCount).sum();
-        System.out.println("transfer " + totalCount + " times");
+        System.out.println("normal customer transfer " + totalCount + " times");
 
         int poorTimes = Arrays.stream(customers).mapToInt(Customer::getPoorTimes).sum();
-        System.out.println("total poor times " + poorTimes);
+        System.out.println("normal customer total poor times " + poorTimes);
+
+        EffectiveCustomer[] effectiveCustomers = new EffectiveCustomer[CUSTOMER_NUM];
+        for (int i = 0; i < CUSTOMER_NUM; i++) {
+            //FIXME: recursive pass value, is that ok?
+            effectiveCustomers[i] = new EffectiveCustomer(MONEY, i, effectiveCustomers, THRESHOLD);
+        }
+
+        Arrays.stream(effectiveCustomers).forEach(customer -> new Thread(customer).start());
+
+        Thread.sleep(SLEEP_TIME);
+        Arrays.stream(effectiveCustomers).forEach(EffectiveCustomer::stop);
+
+        sum = Arrays.stream(effectiveCustomers).mapToInt(EffectiveCustomer::getMoney).sum();
+        System.out.println("effective customer sum is " + sum);
+        assert sum == MONEY*CUSTOMER_NUM;
+
+        totalCount = Arrays.stream(effectiveCustomers).mapToInt(EffectiveCustomer::getCount).sum();
+        System.out.println("effective customer transfer " + totalCount + " times");
+        poorTimes = Arrays.stream(effectiveCustomers).mapToInt(EffectiveCustomer::getPoorTimes).sum();
+        System.out.println("effective customer total poor times " + poorTimes);
 
     }
 
